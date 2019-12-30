c562e93041a0fa39c30ea331f51e666899eee20f
Hong Phuc <og.viet.003.dev@gmail.com>
2019-12-30 17:07:14 +0700

    Making an interactive component - Update

--- src/components/Game/Board/Board.jsx
+++ src/components/Game/Board/Board.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handeClick(i)}
+            />;
   }
 
   render() {
--- src/components/Game/Board/Square/Square.jsx
+++ src/components/Game/Board/Square/Square.jsx
@@ -11,8 +11,11 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={ () => alert('click') }>
-        { this.props.value }
+      <button 
+          className="square" 
+          onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
