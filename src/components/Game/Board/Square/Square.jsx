d31c543576686fc2bbcb808091d43fe32fc44529
Hong Phuc <og.viet.003.dev@gmail.com>
2019-12-30 16:31:14 +0700

    Making an interactive component - Add constructor

---
+++ src/components/Game/Board/Square/Square.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Square extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: null
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={ () => alert('click') }>
+        { this.props.value }
+      </button>
+    );
+  }
+}
+
+export default Square;
