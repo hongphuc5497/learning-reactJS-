50bb3cce7731c9f007d9309f6af3726e81f4cfda
Hong Phuc <og.viet.003.dev@gmail.com>
2019-12-31 14:15:03 +0700

    Adding player O

--- src/components/Game/Board/Board.jsx
+++ src/components/Game/Board/Board.jsx
@@ -6,14 +6,19 @@ class Board extends React.Component {
     super();
 
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true, 
     };
   }
 
   handeClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });  
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({ 
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
   
   renderSquare(i) {
@@ -24,7 +29,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
